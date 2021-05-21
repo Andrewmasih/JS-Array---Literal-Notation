6588d990a059653edfc7674014c4bd454d12e2a4
Andrew Masih <armasih@outlook.com>
2021-05-21 16:11:30 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+/* literal notation is where you define a new array using just empty brackets. Example */
+
+let myArray = ["stringValues", "numberValues", "multipleValues", 100, 101, 5, (5*10) + 2, "anyValue"]
+
+console.log(myArray);
+
+/* It is the "new" way of defining arrays, and I suppose it is shorter/cleaner. */
+
